911195971ff6259995d53cad89cf896a92df532c
Supachai <supachai@CSIGROUPS.COM>
2025-02-07 15:40:35 +0700

    update  UI  PMS 020 030 050  070 080 100

--- Services/Authentication/Controllers/MESSystemController.cs
+++ Services/Authentication/Controllers/MESSystemController.cs
@@ -7,15 +7,18 @@ namespace Authentication.Controllers
     [Route("messystem")]
     public class MESSystemController : ControllerBase
     {
-        private readonly IMESScreenService service;
+        private readonly IMESScreenService service_Screen;
+        private readonly IResouresService service_Resoures;
         private readonly ILogger<MESSystemController> logger;
 
         public MESSystemController(
-            IMESScreenService service,
+            IMESScreenService service_Screen,
+            IResouresService service_Resoures,
             ILogger<MESSystemController> logger
         )
         {
-            this.service = service;
+            this.service_Screen = service_Screen;
+            this.service_Resoures = service_Resoures;
             this.logger = logger;
         }
 
@@ -26,10 +29,28 @@ namespace Authentication.Controllers
         public async Task<IActionResult> GetScreen(MESScreenCriteria criteria)
         {
             // LocalizedMessagesCriteria localizedMessagesCriteria = new LocalizedMessagesCriteria();
-            var results = await service.getMesScreen(criteria);
+            var results = await service_Screen.getMesScreen(criteria);
             return Ok(results);
         }
 
+        [HttpGet]
+        [Route("getresources")]
+        //[TypeFilter(typeof(ActionExceptionFilter))]
+        public IActionResult getResources()
+        {
+            // LocalizedMessagesCriteria localizedMessagesCriteria = new LocalizedMessagesCriteria();
+            var results = service_Resoures.LocalizedResources(null);
+            return Ok(results);
+        }
+        [HttpGet]
+        [Route("getmessages")]
+        //[TypeFilter(typeof(ActionExceptionFilter))]
+        public async Task<IActionResult> getMessages()
+        {
+            // LocalizedMessagesCriteria localizedMessagesCriteria = new LocalizedMessagesCriteria();
+            var results = await service_Resoures.LocalizedMessages(null);
+            return Ok(results);
+        }
 
 
     }
